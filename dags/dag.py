40a70b8a517b177d1050a7e8fb8f9cbc87614fc9
Adegoju5 <115003200+Adegoju5@users.noreply.github.com>
2023-04-24 19:52:02 +0200

    file changes

--- dags/dag.py
+++ dags/dag.py
@@ -1,27 +1,23 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from datetime import datetime, timedelta
+from datetime import datetime
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 4, 20),
-    'email': ['adegoju_a@yahoo.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2023, 4, 21)
 }
 
 dag = DAG(
-    'my_first_dag',
+    'test_airflow',
     default_args=default_args,
-    description='A simple tutorial DAG',
-    schedule_interval=timedelta(days=1),
+    schedule_interval=None
 )
 
-task1 = BashOperator(
-    task_id='task_1',
-    bash_command='echo "Hello guys"',
-    dag=dag,
+check_airflow_task = BashOperator(
+    task_id='check_airflow',
+    bash_command='echo "Airflow is jonzing"',
+    dag=dag
 )
+
+check_airflow_task
