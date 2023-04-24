e69917b27b39d27a6fa2625a82ac4b34f65eab63
Adegoju5 <115003200+Adegoju5@users.noreply.github.com>
2023-04-24 18:36:21 +0200

    changes2

---
+++ DAGS/dag.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 4, 20),
+    'email': ['adegoju_a@yahoo.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'my_first_dag',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+task1 = BashOperator(
+    task_id='task_1',
+    bash_command='echo "Hello World"',
+    dag=dag,
+)
